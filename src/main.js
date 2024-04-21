94e15019cb399e3b39e95fdd04a34a6b0c6b756f
大老师 <523900780@qq.com>
2024-04-21 23:29:34 +0800

    静态资源创建

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import "./assets/main.css";
+// 引入初始化样式文件
+import "@/styles/common.scss";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
@@ -11,7 +12,6 @@ getCategoryAPI().then((res) => {
   console.log(res);
 });
 const app = createApp(App);
-
 app.use(createPinia());
 app.use(router);
 
